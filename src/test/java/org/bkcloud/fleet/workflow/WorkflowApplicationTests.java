c208bd95c53fb5c11333fef49de4958d94253df0
BruceKangCN <brucekangcn@gmail.com>
2021-05-23 13:04:46 +0800

    initial commit

---
+++ src/test/java/org/bkcloud/fleet/workflow/WorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bkcloud.fleet.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
