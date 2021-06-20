80c8a7845f781a6081f126af0522289670ceb721
BruceKangCN <brucekangcn@gmail.com>
2021-06-20 11:45:36 +0800

    fix test issues

--- src/test/java/org/bkcloud/fleet/workflow/WorkflowApplicationTests.java
+++ src/test/java/org/bkcloud/fleet/workflow/WorkflowApplicationTests.java
@@ -1,15 +1,10 @@
 package org.bkcloud.fleet.workflow;
 
-import org.bkcloud.fleet.workflow.process.DeploymentTests;
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
-        DeploymentTests.class,
-})
+@SpringBootTest
 class WorkflowApplicationTests {
 
     @Test
--- src/test/java/org/bkcloud/fleet/workflow/process/DeploymentTests.java
+++ src/test/java/org/bkcloud/fleet/workflow/process/DeploymentTests.java
@@ -2,13 +2,10 @@ package org.bkcloud.fleet.workflow.process;
 
 import org.camunda.bpm.engine.RuntimeService;
 import org.junit.jupiter.api.Assertions;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class DeploymentTests {
 
