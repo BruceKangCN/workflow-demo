52c5520039d214bf72edbc81a063306c38f220da
BruceKangCN <brucekangcn@gmail.com>
2021-06-20 12:16:58 +0800

    add CORS config

--- src/main/java/org/bkcloud/fleet/workflow/WorkflowApplication.java
+++ src/main/java/org/bkcloud/fleet/workflow/WorkflowApplication.java
@@ -3,6 +3,9 @@ package org.bkcloud.fleet.workflow;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableProcessApplication // enable camunda process application
@@ -10,6 +13,17 @@ public class WorkflowApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WorkflowApplication.class, args);
+    }
+
+    // CORS configuration, convenience for development
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/").allowedOrigins("*");
+            }
+        };
     }
 
 }
