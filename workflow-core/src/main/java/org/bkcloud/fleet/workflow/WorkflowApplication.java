4971df99fba67d03fec86a32f84fbc56a67eb410
BruceKangCN <brucekangcn@gmail.com>
2021-07-04 05:11:18 +0800

    fix CORS issue

--- workflow-core/src/main/java/org/bkcloud/fleet/workflow/WorkflowApplication.java
+++ workflow-core/src/main/java/org/bkcloud/fleet/workflow/WorkflowApplication.java
@@ -3,10 +3,12 @@ package org.bkcloud.fleet.workflow;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -17,15 +19,19 @@ public class WorkflowApplication {
         SpringApplication.run(WorkflowApplication.class, args);
     }
 
-    // CORS configuration, convenience for development
+    // CORS configuration
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/").allowedOrigins("*");
-            }
-        };
-    }
+    public FilterRegistrationBean<CorsFilter> processCorsFilter() {
+       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+       CorsConfiguration config = new CorsConfiguration();
+       // config.setAllowCredentials(true);
+       config.addAllowedOrigin("*");
+       config.addAllowedHeader("*");
+       config.addAllowedMethod("*");
+       source.registerCorsConfiguration("/**", config);
 
+       FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+       bean.setOrder(0);
+       return bean;
+    }
 }
